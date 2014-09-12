adda07b5b632c9f454abde81f3b89d47d07b8a16
Abhishek Agrawal <abhisheka@sizesuppose-lm.corp.yahoo.com>
2014-09-11 17:33:27 -0700

    add search view on action bar

--- gen/com/yahoo/abhisheka/gridimagesearch/R.java
+++ gen/com/yahoo/abhisheka/gridimagesearch/R.java
@@ -17,19 +17,19 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int ic_settings=0x7f020001;
+        public static final int ic_search=0x7f020001;
+        public static final int ic_settings=0x7f020002;
     }
     public static final class id {
         public static final int btSave=0x7f080009;
-        public static final int btSearch=0x7f08000c;
-        public static final int etQuery=0x7f08000b;
         public static final int etSiteFilter=0x7f080008;
-        public static final int gvResults=0x7f08000d;
+        public static final int gvResults=0x7f08000b;
         public static final int ivResult=0x7f08000a;
-        public static final int miSettings=0x7f08000e;
+        public static final int miSettings=0x7f08000d;
         public static final int spColorFilter=0x7f080004;
         public static final int spImageSize=0x7f080002;
         public static final int spImageType=0x7f080006;
+        public static final int svActionSearch=0x7f08000c;
         public static final int tvColorFilter=0x7f080003;
         public static final int tvImageSize=0x7f080001;
         public static final int tvImageType=0x7f080005;
